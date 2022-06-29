1e201d8a0f0e58582abb5b2e2a578864f997135d
Ubuntu <ubuntu@ip-172-31-25-70.us-east-2.compute.internal>
2022-06-29 23:59:09 +0000

    Add staging models

---
+++ models/staging/coffee_shop/stg_coffee_shop_orders.sql
@@ -0,0 +1,22 @@
+with source as (
+
+    select * from {{ source('coffee_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        customer_id,
+        total,
+        address,
+        state,
+        zip,
+        created_at as sold_at,
+
+    from source
+
+)
+
+select * from renamed
