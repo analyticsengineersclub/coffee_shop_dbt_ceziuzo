3955114cc156902f9741487aaa3f262bd7abcbf4
Ubuntu <ubuntu@ip-172-31-25-70.us-east-2.compute.internal>
2022-06-30 00:54:41 +0000

    Add weekly revenue by product category

---
+++ models/marts/sales/weekly_revenue_product_category.sql
@@ -0,0 +1,46 @@
+{{ config(
+    materialized='table'
+) }}
+
+
+
+WITH
+
+order_items AS (
+    SELECT * FROM {{ ref('stg_coffee_shop_order_items') }}
+),
+
+orders AS (
+    SELECT * FROM {{ ref('stg_coffee_shop_orders') }}
+),
+
+products AS (
+    SELECT * FROM {{ ref('stg_coffee_shop_products') }}
+),
+
+product_prices AS (
+    SELECT * FROM {{ ref('stg_coffee_shop_product_prices') }}
+),
+
+final AS (
+    SELECT
+         date_trunc(date(orders.sold_at), week) AS weekly,
+         products.product_category AS product_category,
+         sum(product_prices.price) AS revenue
+
+FROM order_items
+
+LEFT JOIN orders
+    ON order_items.order_id = orders.order_id
+
+LEFT JOIN products
+    ON order_items.product_id = products.product_id
+
+LEFT JOIN product_prices
+    ON order_items.product_id = product_prices.product_id
+
+GROUP BY 1,2
+ORDER BY 1
+)
+
+SELECT * FROM final
