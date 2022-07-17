52403764ac22cb3d9d4dfcfde13bebcec1e82c26
Ubuntu <ubuntu@ip-172-31-25-70.us-east-2.compute.internal>
2022-07-17 19:13:38 +0000

    added jinja version of the monthly revenue by product category

---
+++ models/marts/sales/mrpc_jinja.sql
@@ -0,0 +1,25 @@
+{{ config(
+    materialized='table'
+) }}
+
+{% set product_category = ["coffee beans", "merch", "brewing supplies"] %}
+
+
+with all_orders as (
+    select * from {{ ref('all_orders') }}
+),
+
+final as (
+      select 
+           date_trunc(sold_at, month) as date_month,
+
+           {% for product_category in product_category %}
+           sum(case when product_category = '{{product_category}}' then price end) as {{ product_category | replace(" ", "_")  }}_amount,
+           {% endfor %}
+
+      from all_orders
+      group by 1
+      order by 1
+)
+
+select * from final
